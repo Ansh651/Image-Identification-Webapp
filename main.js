37e7657985ca0d71359cefa99212feeb17988b32
Ansh651 <72167893+Ansh651@users.noreply.github.com>
2021-03-03 15:47:38 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -10,4 +10,33 @@ Webcam.set({
 });
 Camera = document.getElementById("camera");
 
-Webcam.attatch('#camera');
+Webcam.attach('#camera');
+
+
+function take_Snapshot(){
+    Webcam.set(function (data_uri){
+        document.getElementById("result").innerHTML = '<img id = "captured_image" src="'+data_uri+'"/>';
+    });
+}
+
+console.log('ml5 version:', ml5.version);
+classifier = ml5.imageClassifier('MobileNet', modelLoaded);
+
+function modelLoaded(){
+    console.log('Model Loaded!');
+}
+
+function check(){
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResults);
+}
+
+
+function gotResults(error, results){
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("object_name").innerHTML=results[0].label;
+    }
+}
